2d675e49c8df27b1fd7af35140c633db4bae0454
LeeWyatt <leewyatt7788@gmail.com>
2023-04-14 15:35:20 +0900

    Add dark and light mode for TopMenuBar

--- components/src/main/java/com/dlsc/jfxcentral2/components/TopMenuBar.java
+++ components/src/main/java/com/dlsc/jfxcentral2/components/TopMenuBar.java
@@ -2,6 +2,12 @@ package com.dlsc.jfxcentral2.components;
 
 import com.dlsc.gemsfx.SearchField;
 import javafx.application.Platform;
+import javafx.css.CssMetaData;
+import javafx.css.PseudoClass;
+import javafx.css.Styleable;
+import javafx.css.StyleableObjectProperty;
+import javafx.css.StyleableProperty;
+import javafx.css.converter.EnumConverter;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
@@ -12,15 +18,30 @@ import javafx.scene.layout.StackPane;
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.kordamp.ikonli.materialdesign2.MaterialDesignA;
 
-public class TopMenuBar extends PaneBase {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class TopMenuBar extends PaneBase {
+    private static final BackgroundMode DEFAULT_BACKGROUND_MODE = BackgroundMode.DARK;
+    private static final PseudoClass LIGHT_PSEUDOCLASS_STATE = PseudoClass.getPseudoClass("light");
+    private static final PseudoClass DARK_PSEUDOCLASS_STATE = PseudoClass.getPseudoClass("dark");
     private final CustomImageView dukeView;
     private final SearchField<String> searchField;
     private final HBox contentBox;
     private Node searchTextField;
 
+    public enum BackgroundMode {
+        LIGHT,
+        DARK
+    }
+
     public TopMenuBar() {
         getStyleClass().add("top-menu-bar");
+
+        activateBackgroundModePseudoClass();
+        backgroundModeProperty().addListener(it -> activateBackgroundModePseudoClass());
+
         contentBox = new HBox();
         contentBox.getStyleClass().add("content");
         getChildren().add(contentBox);
@@ -32,6 +53,12 @@ public class TopMenuBar extends PaneBase {
         searchField = new SearchField<>();
         searchField.setPromptText("Search");
         layoutBySize();
+    }
+
+    private void activateBackgroundModePseudoClass() {
+        BackgroundMode mode = getBackgroundMode();
+        pseudoClassStateChanged(LIGHT_PSEUDOCLASS_STATE, mode == BackgroundMode.LIGHT);
+        pseudoClassStateChanged(DARK_PSEUDOCLASS_STATE, mode == BackgroundMode.DARK);
     }
 
     private Region createSpacingRegion() {
@@ -118,6 +145,69 @@ public class TopMenuBar extends PaneBase {
             logoBox.getStyleClass().add("logo-box");
             return logoBox;
         }
+    }
+
+    private final StyleableObjectProperty<BackgroundMode> backgroundMode = new StyleableObjectProperty<>(DEFAULT_BACKGROUND_MODE) {
+        @Override
+        public Object getBean() {
+            return TopMenuBar.this;
+        }
+
+        @Override
+        public String getName() {
+            return "backgroundMode";
+        }
+
+        @Override
+        public CssMetaData<? extends Styleable, BackgroundMode> getCssMetaData() {
+            return StyleableProperties.BACKGROUND_MODE;
+        }
+    };
+
+    public final StyleableObjectProperty<BackgroundMode> backgroundModeProperty() {
+        return backgroundMode;
+    }
+
+    public final BackgroundMode getBackgroundMode() {
+        return backgroundMode.get();
+    }
+
+    public final void setBackgroundMode(final BackgroundMode backgroundMode) {
+        this.backgroundModeProperty().set(backgroundMode);
+    }
+
+    private static class StyleableProperties {
+
+        private static final CssMetaData<TopMenuBar, BackgroundMode> BACKGROUND_MODE = new CssMetaData<>(
+                "-fx-background-mode", new EnumConverter<>(BackgroundMode.class), DEFAULT_BACKGROUND_MODE) {
+
+            @Override
+            public boolean isSettable(TopMenuBar control) {
+                return !control.backgroundMode.isBound();
+            }
+
+            @Override
+            public StyleableProperty<BackgroundMode> getStyleableProperty(TopMenuBar control) {
+                return control.backgroundModeProperty();
+            }
+        };
+
+        private static final List<CssMetaData<? extends Styleable, ?>> STYLEABLES;
+
+        static {
+            final List<CssMetaData<? extends Styleable, ?>> styleables = new ArrayList<>(PaneBase.getClassCssMetaData());
+            styleables.add(BACKGROUND_MODE);
+            STYLEABLES = Collections.unmodifiableList(styleables);
+        }
+    }
+
+    @Override
+    public List<CssMetaData<? extends Styleable, ?>> getCssMetaData() {
+        return getClassCssMetaData();
+    }
+
+    public static List<CssMetaData<? extends Styleable, ?>> getClassCssMetaData() {
+        return StyleableProperties.STYLEABLES;
     }
 
 }
