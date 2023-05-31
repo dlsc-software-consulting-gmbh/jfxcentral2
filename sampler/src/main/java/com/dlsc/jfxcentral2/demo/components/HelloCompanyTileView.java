20c43890fae7942c08e84d8e4305597a3ecd3014
LeeWyatt <leewyatt7788@gmail.com>
2023-06-01 02:54:45 +0900

    TileView images always have to cover available space

--- sampler/src/main/java/com/dlsc/jfxcentral2/demo/components/HelloCompanyTileView.java
+++ sampler/src/main/java/com/dlsc/jfxcentral2/demo/components/HelloCompanyTileView.java
@@ -3,6 +3,7 @@ package com.dlsc.jfxcentral2.demo.components;
 import com.dlsc.jfxcentral.data.model.Company;
 import com.dlsc.jfxcentral2.components.tiles.CompanyTileView;
 import com.dlsc.jfxcentral2.demo.JFXCentralSampleBase;
+import javafx.scene.Node;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 
@@ -14,6 +15,11 @@ public class HelloCompanyTileView extends JFXCentralSampleBase {
         company.setDescription("DLSC is a software company located in Zurich, Switzerland. They are specialised in developing JavaFX and Swing frameworks and providing consulting services. DLSC is best known for its professional Gantt chart, timeline, and calendar controls for planning and scheduling applications. DLSC is the founder and organizer of the annual");
         CompanyTileView companyTileView = new CompanyTileView(company);
         return new StackPane(companyTileView);
+    }
+
+    @Override
+    public Node getControlPanel() {
+        return super.getControlPanel();
     }
 
     @Override
