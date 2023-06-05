2607a618aa0eb783877c3b0a9e36a2491dde4ebe
LeeWyatt <leewyatt7788@gmail.com>
2023-06-06 01:41:16 +0900

    Integrate available downloads into DownloadOverviewBox

--- components/src/main/java/com/dlsc/jfxcentral2/components/DownloadsBox.java
+++ components/src/main/java/com/dlsc/jfxcentral2/components/DownloadsBox.java
@@ -1,80 +1,151 @@
 package com.dlsc.jfxcentral2.components;
 
-import com.dlsc.jfxcentral.data.ImageManager;
 import com.dlsc.jfxcentral.data.model.Download;
 import com.dlsc.jfxcentral2.utils.IkonUtil;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
 import one.jpro.routing.LinkUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.kordamp.ikonli.Ikon;
+import org.kordamp.ikonli.antdesignicons.AntDesignIconsFilled;
 import org.kordamp.ikonli.javafx.FontIcon;
+import org.kordamp.ikonli.runestroicons.Runestroicons;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DownloadsBox extends PaneBase {
 
-    private final CustomImageView imageView;
-    private final Label nameLabel;
-    private final VBox contentBox;
+    private enum OS {
+        WINDOWS("windows"),
+        MACOS("mac"),
+        LINUX("linux"),
+        GENERIC("generic");
+
+        private final String name;
+
+        OS(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 
     public DownloadsBox(Download download) {
         getStyleClass().addAll("downloads-box");
 
-        imageView = new CustomImageView();
-        imageView.managedProperty().bind(imageView.visibleProperty());
-        imageView.visibleProperty().bind(imageView.imageProperty().isNotNull());
-
-        nameLabel = new Label();
-        nameLabel.setWrapText(true);
-        nameLabel.getStyleClass().add("name");
-
-        contentBox = new VBox(imageView, nameLabel);
-        contentBox.getStyleClass().add("content-box");
-
-        setMaxHeight(Region.USE_PREF_SIZE);
-        getChildren().setAll(contentBox);
-
-        Rectangle clip = new Rectangle();
-        clip.setArcWidth(20);
-        clip.setArcHeight(20);
-        clip.widthProperty().bind(widthProperty());
-        clip.heightProperty().bind(heightProperty());
-        setClip(clip);
-
-        if (imageView.imageProperty().isBound()) {
-            imageView.imageProperty().unbind();
+        List<Download.DownloadFile> downloadFiles = download.getFiles();
+        List<Button> downloadButtons = new ArrayList<>();
+        if (downloadFiles != null && !downloadFiles.isEmpty()) {
+            for (Download.DownloadFile downloadFile : downloadFiles) {
+                Button downloadButton = new Button();
+                downloadButton.getStyleClass().add("download-button");
+                String downloadName = downloadFile.getName();
+                Ikon downloadIcon;
+                if (StringUtils.containsIgnoreCase(downloadName, OS.WINDOWS.getName())) {
+                    downloadButton.getStyleClass().add("download-button-windows");
+                    downloadIcon = AntDesignIconsFilled.WINDOWS;
+                    downloadButton.setUserData(OS.WINDOWS);
+                } else if (StringUtils.containsIgnoreCase(downloadName, OS.MACOS.getName())) {
+                    downloadButton.getStyleClass().add("download-button-mac");
+                    downloadIcon = AntDesignIconsFilled.APPLE;
+                    downloadButton.setUserData(OS.MACOS);
+                } else if (StringUtils.containsIgnoreCase(downloadName, OS.LINUX.getName())) {
+                    downloadButton.getStyleClass().add("download-button-linux");
+                    downloadIcon = Runestroicons.LINUX;
+                    downloadButton.setUserData(OS.LINUX);
+                } else {
+                    downloadButton.getStyleClass().add("download-button-generic");
+                    downloadIcon = IkonUtil.download;
+                    downloadButton.setUserData(OS.GENERIC);
+                }
+                downloadButton.setText(downloadName);
+                downloadButton.setGraphic(new FontIcon(downloadIcon));
+                downloadButton.setMaxWidth(Double.MAX_VALUE);
+                downloadButton.setWrapText(true);
+                LinkUtil.setExternalLink(downloadButton, downloadFile.getUrl());
+                downloadButtons.add(downloadButton);
+            }
         }
 
-        imageView.imageProperty().bind(ImageManager.getInstance().downloadBannerImageProperty(download));
+        Map<OS, List<Button>> groupedButtons = downloadButtons.stream()
+                .collect(Collectors.groupingBy(button -> (OS) button.getUserData()));
 
-        nameLabel.setText(download.getName());
-        List<Download.DownloadFile> downloadFiles = download.getFiles();
-        if (downloadFiles != null && !downloadFiles.isEmpty()) {
-            downloadFiles.forEach(downloadFile -> {
-                Download.FileType fileType = downloadFile.getFileType();
+        VBox mainContainer = new VBox();
+        mainContainer.getStyleClass().add("main-container");
 
-                String downloadInfo = downloadFile.getFileName();
+        if (groupedButtons.containsKey(OS.WINDOWS)) {
+            createDownloadPane(groupedButtons.get(OS.WINDOWS), mainContainer, OS.WINDOWS);
+        }
 
-                if (fileType != null) {
-                    downloadInfo = switch (fileType) {
-                        case DMG -> "DMG for Mac";
-                        case PKG -> "PKG for Mac";
-                        case MSI -> "MSI for Windows";
-                        case EXE -> "EXE for Windows";
-                        case RPM -> "RPM for Linux";
-                        case DEB -> "DEB for Linux";
-                        default -> "Download " + fileType.name().toUpperCase() + " File";
-                    };
-                }
+        if (groupedButtons.containsKey(OS.MACOS)) {
+            createDownloadPane(groupedButtons.get(OS.MACOS), mainContainer, OS.MACOS);
+        }
 
-                Button downloadButton = new Button(downloadInfo, new FontIcon(IkonUtil.download));
-                downloadButton.getStyleClass().add("download-button");
-                LinkUtil.setExternalLink(downloadButton, downloadFile.getUrl());
+        if (groupedButtons.containsKey(OS.LINUX)) {
+            createDownloadPane(groupedButtons.get(OS.LINUX), mainContainer, OS.LINUX);
+        }
 
-                contentBox.getChildren().add(downloadButton);
+        if (groupedButtons.containsKey(OS.GENERIC)) {
+            createDownloadPane(groupedButtons.get(OS.GENERIC), mainContainer, OS.GENERIC);
+        }
+
+        Header header = new Header();
+        header.setTitle("Download Links");
+        header.setIcon(IkonUtil.download);
+
+        Label nameLabel = new Label(download.getName());
+        nameLabel.getStyleClass().add("download-name");
+
+        HBox tagsBox = new HBox();
+        tagsBox.getStyleClass().add("tags-box");
+        String tags = download.getTags();
+        if (StringUtils.isNotBlank(tags)) {
+            Arrays.stream(tags.trim().split(",")).limit(6).forEach(tag -> {
+                Label tagLabel = new Label(tag.trim());
+                tagLabel.getStyleClass().addAll("download-tag", "badge");
+                tagsBox.getChildren().add(tagLabel);
             });
         }
+        Region dividingLine = new Region();
+        dividingLine.getStyleClass().add("dividing-line");
+
+        HBox topBox = new HBox(nameLabel, new Spacer(), tagsBox);
+        topBox.getStyleClass().add("top-box");
+
+        VBox centerBox = new VBox(topBox, mainContainer);
+        centerBox.getStyleClass().add("center-box");
+
+        VBox contentBox = new VBox(header, topBox,mainContainer);
+        contentBox.getStyleClass().add("content-box");
+
+        getChildren().setAll(contentBox);
+    }
+
+    private void createDownloadPane(List<Button> buttons, VBox mainContainer, OS os) {
+        Label titleLabel = new Label(os.getName(), new FontIcon());
+        titleLabel.getStyleClass().addAll("os-label", os.getName() + "-os-label");
+
+        Region dividingLine = new Region();
+        dividingLine.getStyleClass().add("dividing-line");
+        HBox.setHgrow(dividingLine, Priority.ALWAYS);
+        HBox titleWrapper = new HBox(titleLabel, dividingLine);
+        titleWrapper.getStyleClass().add("title-wrapper");
+
+        TilePane tilePane = new TilePane();
+        tilePane.getStyleClass().add("tile-pane");
+        tilePane.getChildren().addAll(buttons);
+
+        mainContainer.getChildren().addAll(titleWrapper, tilePane);
     }
 }
