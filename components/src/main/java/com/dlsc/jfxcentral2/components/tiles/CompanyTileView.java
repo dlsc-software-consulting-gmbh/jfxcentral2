c38ee115c90d84ce324fac4e1808062bcd1c9870
LeeWyatt <leewyatt7788@gmail.com>
2023-06-08 01:56:01 +0900

    Fix: Company logos;

--- components/src/main/java/com/dlsc/jfxcentral2/components/tiles/CompanyTileView.java
+++ components/src/main/java/com/dlsc/jfxcentral2/components/tiles/CompanyTileView.java
@@ -1,7 +1,10 @@
 package com.dlsc.jfxcentral2.components.tiles;
 
 import com.dlsc.jfxcentral.data.model.Company;
+import com.dlsc.jfxcentral2.components.CustomImageView;
 import com.dlsc.jfxcentral2.utils.IkonUtil;
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
 import one.jpro.routing.LinkUtil;
 import org.kordamp.ikonli.javafx.FontIcon;
 
@@ -13,5 +16,19 @@ public class CompanyTileView extends TileView<Company> {
         setButton1Text("HOMEPAGE");
         setButton1Graphic(new FontIcon(IkonUtil.link));
         LinkUtil.setLink(getButton1(), "/companies/" + company.getId());
+    }
+
+    protected Node createFrontTop() {
+        //company logo
+        CustomImageView logoView = new CustomImageView();
+        logoView.imageProperty().bind(imageProperty());
+
+        StackPane imageContainer = new StackPane();
+        imageContainer.getStyleClass().add("image-container");
+        imageContainer.getChildren().setAll(logoView);
+        logoView.fitWidthProperty().bind(imageContainer.widthProperty().subtract(20));
+        logoView.fitHeightProperty().bind(imageContainer.heightProperty().subtract(20));
+
+        return imageContainer;
     }
 }
