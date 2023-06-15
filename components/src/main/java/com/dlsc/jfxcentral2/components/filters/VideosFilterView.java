5bd4bfa9e3617a28607cf3249bfc7f285532ecb4
LeeWyatt <leewyatt7788@gmail.com>
2023-06-15 14:30:56 +0900

    1. Fixed FooterView cannot be centered. 2. Add All logos 3. Add prompt text 4. Fixed Filter view in icon detail page grows without limits 5. Fixed In "small" size the filter view in the openjfx page does not grow 6. Video search filter view needs different filters 7. Fixed At large sizes, the search box disappears after losing focus. 8. Finish styling of search field

--- components/src/main/java/com/dlsc/jfxcentral2/components/filters/VideosFilterView.java
+++ components/src/main/java/com/dlsc/jfxcentral2/components/filters/VideosFilterView.java
@@ -1,39 +1,82 @@
 package com.dlsc.jfxcentral2.components.filters;
 
 import com.dlsc.jfxcentral.data.model.Video;
+import org.apache.commons.lang3.StringUtils;
 
-import java.time.LocalDate;
 import java.util.List;
 
-public class VideosFilterView extends SearchFilterView<Video> {
+public class VideosFilterView extends SimpleSearchFilterView<Video> {
 
     public VideosFilterView() {
         getStyleClass().add("videos-filter-view");
-        setOnSearch(null);
+        setSortGroup(null);
+        setSearchPromptText("Search for a video");
 
         getFilterGroups().setAll(
-                new FilterGroup<>("LENGTH", List.of(
+                new FilterGroup<>("Type", List.of(
                         new FilterItem<>("ALL", item -> true, true),
-                        new FilterItem<>("Less than 5 mins", item -> item.getMinutes() < 5),
-                        new FilterItem<>("5-15 min", item -> item.getMinutes() >= 5 && item.getMinutes() < 15),
-                        new FilterItem<>("15-60 min", item -> item.getMinutes() >= 15 && item.getMinutes() < 60),
-                        new FilterItem<>("More than 60 mins", item -> item.getMinutes() >= 60))),
-                new FilterGroup<>("DATE", List.of(
+                        new FilterItem<>("Demo", item -> StringUtils.equalsIgnoreCase(item.getType(), "Demo")),
+                        new FilterItem<>("Keynote", item -> StringUtils.equalsIgnoreCase(item.getType(), "Keynote")),
+                        new FilterItem<>("Library", item -> StringUtils.equalsIgnoreCase(item.getType(), "Library")),
+                        new FilterItem<>("Presentation", item -> StringUtils.equalsIgnoreCase(item.getType(), "Presentation")),
+                        new FilterItem<>("Real World Application", item -> StringUtils.equalsIgnoreCase(item.getType(), "Real World Application")),
+                        new FilterItem<>("Roadmap", item -> StringUtils.equalsIgnoreCase(item.getType(), "Roadmap")),
+                        new FilterItem<>("Tips & Tricks", item -> StringUtils.equalsIgnoreCase(item.getType(), "Tips & Tricks")),
+                        new FilterItem<>("Tool", item -> StringUtils.equalsIgnoreCase(item.getType(), "Tool")),
+                        new FilterItem<>("Tutorial", item -> StringUtils.equalsIgnoreCase(item.getType(), "Tutorial"))
+                )),
+                new FilterGroup<>("Event", List.of(
                         new FilterItem<>("ALL", item -> true, true),
-                        new FilterItem<>("Less than a week ago", item -> item.getPublishedOn().isAfter(LocalDate.now().minusDays(7))),
-                        new FilterItem<>("Less than a month ago", item -> item.getPublishedOn().isAfter(LocalDate.now().minusMonths(1))),
-                        new FilterItem<>("Less than 3 month ago", item -> item.getPublishedOn().isAfter(LocalDate.now().minusMonths(3))),
-                        new FilterItem<>("Less than 6 month ago", item -> item.getPublishedOn().isAfter(LocalDate.now().minusMonths(3))),
-                        new FilterItem<>("Less than a year ago", item -> item.getPublishedOn().isAfter(LocalDate.now().minusYears(1))))),
-                new FilterGroup<>("TYPE", List.of(
+                        //new FilterItem<>("", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "")),
+                        new FilterItem<>("Devoxx", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "Devoxx")),
+                        new FilterItem<>("JChampions Conf", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "JChampions Conf")),
+                        new FilterItem<>("JFX Days 2018", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "JFX Days 2018")),
+                        new FilterItem<>("JFX Days 2019", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "JFX Days 2019")),
+                        new FilterItem<>("JFX Days 2020", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "JFX Days 2020")),
+                        new FilterItem<>("JavaOne", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "JavaOne")),
+                        new FilterItem<>("JavaOne 2014", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "JavaOne 2014")),
+                        new FilterItem<>("Oracle CodeOne 2019", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "Oracle CodeOne 2019")),
+                        new FilterItem<>("Oracle Learning", item -> StringUtils.equalsIgnoreCase(item.getEvent(), "Oracle Learning"))
+                )),
+                new FilterGroup<>("Domain", List.of(
                         new FilterItem<>("ALL", item -> true, true),
-                        new FilterItem<>("Interview", item -> true),
-                        new FilterItem<>("Tutorial", item -> true),
-                        new FilterItem<>("Discussion", item -> true),
-                        new FilterItem<>("Conference", item -> true),
-                        new FilterItem<>("Keynote", item -> true)))
+                        //new FilterItem<>("", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "")),
+                        //new FilterItem<>("46", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "46")),
+                        new FilterItem<>("Aircraft design", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Aircraft design")),
+                        new FilterItem<>("CAD", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "CAD")),
+                        new FilterItem<>("CRM", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "CRM")),
+                        new FilterItem<>("Charting", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Charting")),
+                        new FilterItem<>("Charts", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Charts")),
+                        new FilterItem<>("Custom Control", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Custom Control")),
+                        new FilterItem<>("Custom Controls", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Custom Controls")),
+                        new FilterItem<>("Demo", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Demo")),
+                        new FilterItem<>("Desktop Search", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Desktop Search")),
+                        new FilterItem<>("Education", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Education")),
+                        new FilterItem<>("Energy", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Energy")),
+                        new FilterItem<>("Gaming", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Gaming")),
+                        new FilterItem<>("IDE", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "IDE")),
+                        new FilterItem<>("Icons", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Icons")),
+                        new FilterItem<>("Logistics", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Logistics")),
+                        new FilterItem<>("Manufacturing", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Manufacturing")),
+                        new FilterItem<>("Medical", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Medical")),
+                        new FilterItem<>("Planning & Scheduling", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Planning & Scheduling")),
+                        new FilterItem<>("Research", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Research")),
+                        new FilterItem<>("Science", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Science")),
+                        new FilterItem<>("Sports", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Sports")),
+                        new FilterItem<>("Streaming", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Streaming")),
+                        new FilterItem<>("Test Automation", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Test Automation")),
+                        new FilterItem<>("Testing", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Testing")),
+                        new FilterItem<>("Tool", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Tool")),
+                        new FilterItem<>("Tourism", item -> StringUtils.equalsIgnoreCase(item.getDomain(), "Tourism"))
+
+
+                ))
         );
 
+        setOnSearch(text -> video -> StringUtils.isBlank(text)
+                || StringUtils.containsIgnoreCase(video.getName(), text)
+                || StringUtils.containsIgnoreCase(video.getDescription(), text)
+        );
 
     }
 
