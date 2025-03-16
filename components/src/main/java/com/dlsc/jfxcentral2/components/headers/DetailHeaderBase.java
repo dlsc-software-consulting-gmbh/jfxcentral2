71f312b00516731b70eb35514d304e51964abd28
leewyatt <leewyatt7788@gmail.com>
2025-03-16 18:32:48 +0900

    Remove Twitter sharing functionality
    The Twitter share link has been removed from the DetailHeaderBase component. This streamlines the social sharing options and reflects current priorities. Other share links like Facebook and LinkedIn remain unaffected.

--- components/src/main/java/com/dlsc/jfxcentral2/components/headers/DetailHeaderBase.java
+++ components/src/main/java/com/dlsc/jfxcentral2/components/headers/DetailHeaderBase.java
@@ -79,7 +79,6 @@ public class DetailHeaderBase extends CategoryHeader {
             String bodyWithUrlAndTags = URLEncoder.encode(shareText + " https://www.jfx-central.com/" + shareUrl + " #javafx #java #ux #ui", StandardCharsets.UTF_8);
 
             socialLinksView.setFacebookUrl("https://www.facebook.com/sharer/sharer.php?u=" + url + "&t=" + titleWithBody);
-            socialLinksView.setTwitterUrl("https://twitter.com/share?text=" + body + "&url=" + url + "&hashtags=javafx,java,ux,ui");
             socialLinksView.setLinkedInUrl("https://www.linkedin.com/shareArticle?mini=false&url=" + url + "&title=" + title + "&summary=" + body);
             socialLinksView.setMailUrl("mailto:?subject=" + title.replace("+", "%20") + "&body=" + body.replace("+", "%20"));
             socialLinksView.setRedditUrl("https://www.reddit.com/r/JavaFX/submit?title=" + title + "&selftext=true&text=" + bodyWithUrl + "&link=" + url);
