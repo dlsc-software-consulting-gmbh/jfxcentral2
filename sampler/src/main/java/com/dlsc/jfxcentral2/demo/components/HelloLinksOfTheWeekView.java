3e3c282a578a3a69c4e27b2934f396459cb9ee17
leewyatt <leewyatt7788@gmail.com>
2025-03-31 20:36:25 +0900

    Refactor LinksOfTheWeekView for improved data handling and navigation
    Updated LinksOfTheWeekView to initialize with a specific LinksOfTheWeek instance and introduced better sorting and navigation logic. Removed unused properties and redundant methods, simplifying pagination and markdown handling. Additionally, adjusted related components and styles to align with the streamlined design.

--- sampler/src/main/java/com/dlsc/jfxcentral2/demo/components/HelloLinksOfTheWeekView.java
+++ sampler/src/main/java/com/dlsc/jfxcentral2/demo/components/HelloLinksOfTheWeekView.java
@@ -1,5 +1,6 @@
 package com.dlsc.jfxcentral2.demo.components;
 
+import com.dlsc.jfxcentral.data.DataRepository;
 import com.dlsc.jfxcentral.data.model.LinksOfTheWeek;
 import com.dlsc.jfxcentral2.components.LinksOfTheWeekView;
 import com.dlsc.jfxcentral2.components.SizeComboBox;
@@ -9,7 +10,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class HelloLinksOfTheWeekView extends JFXCentralSampleBase {
@@ -18,8 +19,12 @@ public class HelloLinksOfTheWeekView extends JFXCentralSampleBase {
 
     @Override
     protected Region createControl() {
-        view = new LinksOfTheWeekView();
-        view.getLinksOfTheWeeks().setAll(createLinksOfTheWeeks());
+        List<LinksOfTheWeek> links = DataRepository.getInstance().getLinksOfTheWeek();
+        links.sort(Comparator.comparing(LinksOfTheWeek::getCreatedOn).reversed());
+        LinksOfTheWeek latestLotw = links.get(0);
+
+        view = new LinksOfTheWeekView(latestLotw);
+        view.getLinksOfTheWeeks().setAll(links);
         return new ScrollPane(new StackPane(view));
     }
 
@@ -27,28 +32,7 @@ public class HelloLinksOfTheWeekView extends JFXCentralSampleBase {
     public Node getControlPanel() {
         SizeComboBox sizeComboBox = new SizeComboBox();
         view.sizeProperty().bind(sizeComboBox.valueProperty());
-        //weekCount bind size
-        view.weekCountProperty().bind(sizeComboBox.valueProperty().map(size -> size.isLarge() ? 3 : 1));
         return sizeComboBox;
-    }
-
-    private List<LinksOfTheWeek> createLinksOfTheWeeks() {
-        List<LinksOfTheWeek> list = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            LinksOfTheWeek week = new LinksOfTheWeek();
-            week.setName("[" + (i + 1) + "] 16 jan - 22 jan 2023");
-            week.setDescription("""
-                    #### Hello World
-                    Foojay.io, the website for Friends Of OpenJDK, published the podcast ‘The State of JavaFX Framework, Libraries, and Projects’. These guests spoke about the JavaFX framework itself, but also about the libraries and applications that are built with it:
-                    > Pedro Duque Vieira
-                     
-                    Are you considering to write a JavaFX Mastodon client? Take a look at the Bigbone project, that aims to implement the Mastodon API project by André Gasser. \n
-                    Are you considering to write a JavaFX Mastodon client? Take a look at the Bigbone project, that aims to implement the Mastodon API project by André Gasser. \n
-                    """);
-            list.add(week);
-        }
-
-        return list;
     }
 
     @Override
