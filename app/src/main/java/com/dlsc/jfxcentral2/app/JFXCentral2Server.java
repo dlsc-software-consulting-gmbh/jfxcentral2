141dbbbc4ea2514acbd78e2822809f040fb36ac2
dlemmermann <dlemmermann@gmail.com>
2026-03-16 15:36:59 +0100

    Updated `com.dlsc.jfxcentral:model` to version `2.7.1` and added `/sitemap.xml` request handler.

--- app/src/main/java/com/dlsc/jfxcentral2/app/JFXCentral2Server.java
+++ app/src/main/java/com/dlsc/jfxcentral2/app/JFXCentral2Server.java
@@ -1,8 +1,8 @@
 package com.dlsc.jfxcentral2.app;
 
 import com.dlsc.jfxcentral.data.RSSManager;
+import com.dlsc.jfxcentral.data.util.SitemapGenerator;
 import com.jpro.webapi.server.ServerAPI;
-import com.jpro.webapi.server.Request;
 import com.jpro.webapi.server.Response;
 
 public class JFXCentral2Server {
@@ -10,6 +10,12 @@ public class JFXCentral2Server {
         ServerAPI.getServerAPI().addRequestHandler(request -> {
             if (request.getPath().equals("/lotw/rss.xml")) {
                 return Response.of(RSSManager.createRSS().getBytes(), "application/rss+xml");
+            }
+            return Response.empty();
+        });
+        ServerAPI.getServerAPI().addRequestHandler(request -> {
+            if (request.getPath().equals("/sitemap.xml")) {
+                return Response.of(SitemapGenerator.generate().getBytes(), "application/xml");
             }
             return Response.empty();
         });
